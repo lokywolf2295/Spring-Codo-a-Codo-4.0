4dcc6fb462dd1bdaaceb3ec610ab4c975b089ddc
lokywolf2295 <matiasesek1995@gmail.com>
2023-03-06 18:02:58 -0300

    Update: create first Hola Mundo

--- Project1/src/main/java/com/codoacodo/Project1/Project1Application.java
+++ Project1/src/main/java/com/codoacodo/Project1/Project1Application.java
@@ -2,12 +2,17 @@ package com.codoacodo.Project1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class Project1Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Project1Application.class, args);
+
+		System.out.println("Hola Mundo con Spring");
 	}
+
 
 }
